bc3bd5e21e8127b65136a94a948eac5e2ebcd925
Mohammed Nasr <mohammed.nasr@ericsson.com>
2021-07-11 17:13:31 +0200

    Update cpp-demo.cpp

--- cpp-demo.cpp
+++ cpp-demo.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Welcome to our first C++ program!";
+    int user_input;
+    std::cout << "Welcome to our first C++ program! \n";
+    std::cout << "Please type a number between 1 & 100. \n";
+    std::cin >> user_input;
+    std::cout << "Thank you for typing a number between 1 & 100. The number you entered is: "<< user_input << '\n';
     return 0;
 }
