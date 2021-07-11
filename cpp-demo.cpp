ad5dd2936087c0b5707b9f913eee545875d5f6bf
Mohammed Nasr <mohammed.nasr@ericsson.com>
2021-07-11 20:33:52 +0200

    Results function added

--- cpp-demo.cpp
+++ cpp-demo.cpp
@@ -2,11 +2,21 @@
 
 #include <iostream>
 
+
+
+int results(int user_input) {
+    std::cout << "Thank you for typing a number between 1 & 100. The number you entered is: "<< user_input << '\n';
+	return 0;
+}	
+
+
+
 int main() {
     int user_input;
     std::cout << "Welcome to our first C++ program! \n";
     std::cout << "Please type a number between 1 & 100. \n";
     std::cin >> user_input;
-    std::cout << "Thank you for typing a number between 1 & 100. The number you entered is: "<< user_input << '\n';
+    results(user_input);
     return 0;
 }
+
