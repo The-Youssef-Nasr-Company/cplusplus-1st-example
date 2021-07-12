655a4e3c7513806897fd91252ff14573555724c5
Mohammed Nasr <mohammed.nasr@ericsson.com>
2021-07-12 18:11:19 +0200

    Upgrade cpp-demo.cpp to a smart program

--- cpp-demo.cpp
+++ cpp-demo.cpp
@@ -13,10 +13,20 @@ int results(int user_input) {
 
 int main() {
     int user_input;
+    bool loop_condition;
     std::cout << "Welcome to our first C++ program! \n";
-    std::cout << "Please type a number between 1 & 100. \n";
-    std::cin >> user_input;
+    loop_condition=true;
+    while(loop_condition){
+        std::cout << "Please type a number between 1 & 100. \n";
+        std::cin >> user_input;
+        if(user_input > 0 and user_input <= 100){
+            loop_condition=false;
+        }
+        else{
+            std::cout << "The number you typed is not between 1 & 100 because it is wrong. Please try again. \n";
+        }
+        
+    }
     results(user_input);
     return 0;
 }
-
